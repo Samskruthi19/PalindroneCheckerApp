93378e7dbbe9ca65b6dacc6f28b4e69981e99a45
Samskruthi19 <kk6392@srmist.edu.in>
2026-03-12 09:31:57 +0530

    UC8-LinkedListBasedPalindromeChecker

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,29 +1,80 @@
-import java.util.*;
+class Node {
+    char data;
+    Node next;
 
-public class PalindromeCheckerApp {
+    Node(char data) {
+        this.data = data;
+        this.next = null;
+    }
+}
 
-    public static void main(String[] args) {
+public class PalindromeCheckerApp{
 
-        String input = "civic";
+    public static Node createList(String str) {
+        Node head = null, tail = null;
 
-        Queue<Character> queue = new LinkedList<>();
-        Stack<Character> stack = new Stack<>();
+        for (char c : str.toCharArray()) {
+            Node newNode = new Node(c);
 
-        for (char c : input.toCharArray()) {
-            queue.add(c);
-            stack.push(c);
-        }
-
-        boolean isPalindrome = true;
-
-        while (!queue.isEmpty()) {
-            if (queue.remove() != stack.pop()) {
-                isPalindrome = false;
-                break;
+            if (head == null) {
+                head = tail = newNode;
+            } else {
+                tail.next = newNode;
+                tail = newNode;
             }
         }
+        return head;
+    }
 
-        System.out.println("Input : " + input);
-        System.out.println("Is Palindrome? : " + isPalindrome);
+    public static Node reverse(Node head) {
+        Node prev = null;
+        Node current = head;
+        Node next = null;
+
+        while (current != null) {
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        return prev;
+    }
+
+    public static boolean isPalindrome(Node head) {
+        if (head == null || head.next == null) {
+            return true;
+        }
+
+        Node slow = head;
+        Node fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        Node secondHalf = reverse(slow);
+        Node firstHalf = head;
+
+        while (secondHalf != null) {
+            if (firstHalf.data != secondHalf.data) {
+                return false;
+            }
+            firstHalf = firstHalf.next;
+            secondHalf = secondHalf.next;
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String input = "level";
+
+        Node head = createList(input);
+
+        boolean result = isPalindrome(head);
+
+        System.out.println("Input: " + input);
+        System.out.println("Is Palindrome? " + result);
     }
 }
